57efccb4aa8c7bfe5d8ea0efd75af37b6db4c7d7
Lorenzo Ciocca <103076648+lorixhack@users.noreply.github.com>
2025-05-14 13:28:13 +0200

    feat: add script to source ros pkg and update gitignore

---
+++ source_mk2_arm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source ~/arm_ws/install/setup.bash
+chmod +x ~/arm_ws/install/mk2_arm/lib/mk2_arm/joint_publisher_serial.py
+chmod +x ~/arm_ws/install/mk2_arm/lib/mk2_arm/joint_publisher_can.py
+
+exit 0
